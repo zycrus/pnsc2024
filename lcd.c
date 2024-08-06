317097900593e245039254da2c93cf07817396cf
Zycrus <47321670+zycrus@users.noreply.github.com>
2024-08-06 10:44:14 +0800

    Create lcd.c

---
+++ lcd.c
@@ -0,0 +1,26 @@
+void LCD_SendByte(char _byte, int _rs)
+{
+  HAL_GPIO_WritePin(RS_GPIOP_Port, RS_Pin, _rs);
+  
+  HAL_GPIO_WritePin(D4_GPIO_Port, D4_pin, (_byte<<0) & 0x01);
+  HAL_GPIO_WritePin(D4_GPIO_Port, D5_pin, (_byte<<1) & 0x01);
+  HAL_GPIO_WritePin(D4_GPIO_Port, D6_pin, (_byte<<2) & 0x01);
+  HAL_GPIO_WritePin(D4_GPIO_Port, D7_pin, (_byte<<3) & 0x01);
+
+  HAL_GPIO_WritePin(EN_GPIO_Port, EN_Pin, GPIO_PIN_SET);
+  delayMicro(20);
+  HAL_GPIO_WritePin(EN_GPIO_Port, EN_Pin, GPIO_PIN_RESET);
+  delayMicro(20);
+}
+
+void LCD_SendCMD(char _cmd)
+{
+  LCD_SendByte((_cmd<<4) & 0x0f, 1);
+  LCD_SendByte((_cmd) & 0x0f, 1);
+}
+
+void LCD_SendData(char _data)
+{
+  LCD_SendByte((_cmd<<4) & 0x0f, 0);
+  LCD_SendByte((_cmd) & 0x0f, 0);
+}
