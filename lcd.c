4d88123c416df415bc35ea02b2c39898e20bd246
Zycrus <47321670+zycrus@users.noreply.github.com>
2024-08-20 05:13:15 +0800

    Update lcd.c

--- lcd.c
+++ lcd.c
@@ -2,10 +2,10 @@ void LCD_SendByte(char _byte, int _rs)
 {
   HAL_GPIO_WritePin(RS_GPIOP_Port, RS_Pin, _rs);
   
-  HAL_GPIO_WritePin(D4_GPIO_Port, D4_pin, (_byte<<0) & 0x01);
-  HAL_GPIO_WritePin(D4_GPIO_Port, D5_pin, (_byte<<1) & 0x01);
-  HAL_GPIO_WritePin(D4_GPIO_Port, D6_pin, (_byte<<2) & 0x01);
-  HAL_GPIO_WritePin(D4_GPIO_Port, D7_pin, (_byte<<3) & 0x01);
+  HAL_GPIO_WritePin(D4_GPIO_Port, D4_pin, (_byte >> 0) & 0x01);
+  HAL_GPIO_WritePin(D4_GPIO_Port, D5_pin, (_byte >> 1) & 0x01);
+  HAL_GPIO_WritePin(D4_GPIO_Port, D6_pin, (_byte >> 2) & 0x01);
+  HAL_GPIO_WritePin(D4_GPIO_Port, D7_pin, (_byte >> 3) & 0x01);
 
   HAL_GPIO_WritePin(EN_GPIO_Port, EN_Pin, GPIO_PIN_SET);
   HAL_GPIO_WritePin(EN_GPIO_Port, EN_Pin, GPIO_PIN_RESET);
